a74f162c4984a783b056f4717556e1bb1e4f6f09
SimonCao1207 <hainam@kaist.ac.kr>
2024-08-27 11:48:20 +0900

    add transform

--- README.md
+++ README.md
@@ -4,12 +4,10 @@
 
 Datasets: 
 - Shopee (https://www.kaggle.com/competitions/shopee-product-matching/data)
-- CASIA (https://www.kaggle.com/datasets/divg07/casia-20-image-tampering-detection-dataset/data) 
-- VGG2 (https://github.com/ox-vgg/vgg_face2)
-- CELEB-500K (https://github.com/JiajiongCao/CELEB-500K)
 
 TODO: 
 - prepare dataset, implement data loader
 - implement AAM loss
+    (https://github.com/yinguobing/arcface/blob/main/losses.py)
 
 
--- data.py
+++ data.py
@@ -6,6 +6,7 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision
 from torchvision.io import read_image
 import matplotlib.pyplot as plt
+from torchvision import transforms
 from PIL import Image
 
 train_df = pd.read_csv("./data/train.csv")
@@ -14,24 +15,29 @@ train_df["target"] = train_df.label_group.map(tmp)
 BASE = "./data/train_images"
 
 class Shopee(Dataset):
-    def __init__(self, df, img_size=256, path=''):
+    def __init__(self, df, img_size=224, path=''):
         self.df = df
         self.img_size = img_size
         self.path = path
-        self.transform = torchvision.transforms.Resize(img_size)
+        self.transform = transforms.Compose([
+            transforms.Resize(img_size),
+            transforms.ToTensor(),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        ])
     
     def __len__(self):
         return len(self.df)
     
     def __getitem__(self, idx):
         img_path = os.path.join(self.path, self.df.iloc[idx]["image"])
-        image = read_image(img_path)
-        return self.transform(image)
+        image = Image.open(img_path)
+        label_group = self.df.iloc[idx]["label_group"]
+        return self.transform(image), label_group
 
 if __name__ == "__main__":
     train_data = Shopee(train_df, path=BASE)
     train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
-    train_features = next(iter(train_loader))
+    train_features, _ = next(iter(train_loader))
     print(f"Feature batch shape: {train_features.size()}")
     img = train_features[0].squeeze()
     plt.imshow(rearrange(img, 'b w h -> w h b'))
